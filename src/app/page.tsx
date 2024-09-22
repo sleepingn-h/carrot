5d5c301c51f54b9f2993557d6ba219c81d72a000
JooYoung <sleepingz039@gmail.com>
2024-09-22 20:57:57 +0900

    메인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Heading from '@/components/heading/Heading';
 import styles from './page.module.css';
+import Article from '@/components/article/Article';
 
 export default async function Home() {
-  return <>인덱스 페이지</>;
+  return (
+    <>
+      <Heading align='center' title='중고거래 최신매물' className='fz600' />
+      <Article />
+    </>
+  );
 }
